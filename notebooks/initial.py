aa98ab819f6569a324a2425f6aa7d018fbd8ef27
will-sauer <will@heueso.com>
2025-04-18 21:57:56 -0400

    prelim final results - french nuclear extractive intensity per kwh bada boom

--- notebooks/initial.py
+++ notebooks/initial.py
@@ -32,7 +32,7 @@ I = np.identity(len(exio.A)) #* create an identity matrix of the A matrix
 L = L = np.linalg.inv(I - exio.A.values) #* LEONTIEF
 F = exio.satellite.F.values #* load all F values into standalone dataframe
 
-total_intensity_vector = F @ L # * total intensity vector: per 1 M.EUR of nuclear sector(s) output
+total_intensity_vector = F @ L # * total intensity vector: per M.EUR of output
 
 #* add back in names
 total_intensity_vector_w_names = pd.DataFrame(total_intensity_vector, index=exio.satellite.F.index, columns=exio.A.columns)
@@ -48,11 +48,13 @@ uranium_impacts = [
     if 'uranium' in impact.lower()
 ]
 french_nuclear_total_intensity_vector_only_uranium = french_nuclear_total_intensity_vector.loc[uranium_impacts]
+french_nuclear_total_intensity_vector_only_uranium_sum = french_nuclear_total_intensity_vector_only_uranium.to_numpy().sum()
 
 #! change total intensity from per kt extraction / M.EUR to kg extraction / kWh
 
 kg_kt = 1_000_000 #* 1000 kg / 1 kt
 meur_eur = 1 / 1_000_000 #* 1 meur / 1000000 eur
+eur_meur = 1_000_000 #* 1 eur / 1 meur
 french_nuclear_output_2011_kwh = 442_387_000_000 #* iea lists as gwh, converted to kwh
 #* https://www.iea.org/data-and-statistics/data-tools/energy-statistics-data-browser?country=FRANCE&energy=Electricity&year=2011
 
@@ -62,11 +64,17 @@ nuclear_electricity_row = [
     if 'fr' in row[0].lower() and 'production of electricity by nuclear' in row[1].lower()
 ]
 
-#* extract the column from the Y matrix (this will include all final demand rows)
+#* extract the column from the Y matrix (this will include all final demand rows) for the production sector
 #* transpose and sort for easier viewing in datawrangler
 french_nuclear_electricity_final_demand = exio.Y.loc[nuclear_electricity_row, :].transpose()
 french_nuclear_electricity_final_demand = french_nuclear_electricity_final_demand.sort_values(by=french_nuclear_electricity_final_demand.columns[0], ascending=False)
 
-#* sum all rows to get total final demand (small check against the above data frame)
+#* doing the same thing in different way as a check: sum all rows to get total final demand
 french_nuclear_electricity_final_demand_total = french_nuclear_electricity_final_demand.sum(axis=None)
 
+#* 2011 output in euros per kWh
+french_nuclear_output_eur_kwh = french_nuclear_electricity_final_demand_total / french_nuclear_output_2011_kwh * eur_meur
+
+#*so now i have all the elements to do the calculation ... do this upon return
+#* don't forget to conert final demand to eur (from M.EUR?)
+french_extraction_intensity_kg_kwh = french_nuclear_total_intensity_vector_only_uranium_sum * kg_kt * meur_eur * french_nuclear_output_eur_kwh
